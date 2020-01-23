9d0e5a2a51835f2064ba8fef70aff470a6ab5802
meushgram <dhanushkumar@Dhanushs-MacBook-Air.local>
2020-01-23 11:13:24 +0530

    simple program to create hash using

---
+++ imageHash.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import imagehash
+hash = imagehash.average_hash(Image.open('/Users/dhanushkumar/Desktop/Screenshot 2020-01-20 at 12.34.48 PM.png'))
+print(' Hash is + {}'.format(hash))
+otherhash = imagehash.average_hash(Image.open('/Users/dhanushkumar/Desktop/Screenshot 2020-01-20 at 10.53.47 AM.png'))
+print(' Hash is + {}'.format(otherhash))
